b7c3589e2a60a6ccb8d70cdc600c0cd9f2c89a7d
Khoi Hoang <khoi.hoang@bouvet.no>
2023-08-20 02:30:33 +0200

    add: ✨ HashTables for object

---
+++ public/src/HashTables/HashTablesObject.js
@@ -0,0 +1,103 @@
+class HashTable {
+	constructor(fixedSize, numberElements) {
+		if (this._isNumberElement(numberElements)) {
+			this._printErrorMessage('Number of elements required!');
+		}
+
+		this._data = {};
+		this.fixedSize = fixedSize;
+		this.numberElements = numberElements;
+	}
+
+	_isNumberElement(numberElements) {
+		return isNaN(numberElements);
+	}
+
+	getAllBuckets() {
+		return this._data;
+	}
+
+	findValue(valueLookup) {
+		const buckets = this._getBuckets(this._data);
+		const foundBuckets = {};
+
+		for (let i = 0; i < buckets.length; i++) {
+			const bucket = buckets[i];
+
+			for (const [bucketKey, bucketValue] of Object.entries(bucket)) {
+				if (bucketValue === valueLookup) {
+					foundBuckets[bucketKey] = bucketValue;
+				}
+			}
+		}
+
+		return foundBuckets;
+	}
+
+	_getBuckets(bucket) {
+		return Object.entries(bucket).map(([_, bucketItem]) => bucketItem);
+	}
+
+	set(key, value) {
+		const countItems = Object.keys(this._data).length;
+
+		if (!this._isAllowedInsertData(this.fixedSize, countItems)) {
+			this._printErrorMessage('You are not allowed to add more item');
+		}
+
+		const calculatedAllocationAddress = this._getCalculatedAllocationAddress(value);
+
+		const itemPayload = {
+			key,
+			value,
+			calculatedAllocationAddress,
+		};
+
+		this._insertAllocatedItem(itemPayload);
+
+		return this;
+	}
+
+	_isAllowedInsertData(allowedSize, currentData) {
+		return allowedSize > currentData;
+	}
+
+	_getCalculatedAllocationAddress(value) {
+		return value % this.numberElements;
+	}
+
+	_insertAllocatedItem(payload) {
+		const { key, value, calculatedAllocationAddress } = payload;
+
+		this._data = {
+			...this._data,
+			[calculatedAllocationAddress]: {
+				...this._data[calculatedAllocationAddress],
+				[this._hash(key)]: value,
+			},
+		};
+	}
+
+	_hash(key) {
+		return `${key}-${crypto.randomUUID()}`;
+	}
+
+	_printErrorMessage(errorMessage) {
+		throw new Error(errorMessage);
+	}
+}
+
+const hashTable = new HashTable(50, 13);
+
+hashTable.set('grapes', 10_000);
+hashTable.set('grapes', 10_000);
+hashTable.set('grapes', 10_000);
+
+hashTable.set('apple', 50);
+hashTable.set('apple', 50);
+
+hashTable.set('banana', 150);
+
+console.log(hashTable.findValue(10_000));
+
+// console.log(hashTable.find('grapes'));
--- public/src/main.js
+++ public/src/main.js
@@ -1,6 +1,7 @@
 // import './MyArray.js';
 // import './ReverseString.js';
 // import './MergeSortedArray.js';
-import './HashTables.js';
+import './HashTables/HashTablesArray.js';
+import './HashTables/HashTablesObject.js';
 
 // console.log(result);
