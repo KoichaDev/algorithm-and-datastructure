00587899d00fa81e9c8f86d7b5f66ae2ac8a03cb
Khoi Hoang <khoi.hoang@bouvet.no>
2023-08-21 09:59:29 +0200

    add: ✨ Singly LinkedLists for appending

---
+++ public/src/LinkedLists/SinglyLinkedLists.js
@@ -0,0 +1,44 @@
+class SinglyLinkedLists {
+	constructor(value) {
+		this.head = {
+			value: value,
+			id: this.#createMemorySpaceAllocation(),
+			nextNode: null,
+		};
+
+		this.length = 0;
+	}
+
+	insertNextNode(value) {
+		const newNode = {
+			value: value,
+			id: this.#createMemorySpaceAllocation(),
+			nextNode: null,
+		};
+
+		let currentNode = this.head;
+
+		while (currentNode.nextNode !== null) {
+			currentNode = currentNode.nextNode;
+		}
+
+		currentNode.nextNode = newNode;
+
+		this.length++;
+
+		return this;
+	}
+
+	#createMemorySpaceAllocation(minValue = 1000, maxValue = 9999) {
+		const min = minValue;
+		const max = maxValue;
+		const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+		return randomNumber;
+	}
+}
+
+const singlyLinkedLists = new SinglyLinkedLists(2);
+
+singlyLinkedLists.insertNextNode(5).insertNextNode(10).insertNextNode(20);
+
+console.log(singlyLinkedLists);
--- public/src/main.js
+++ public/src/main.js
@@ -2,6 +2,7 @@
 // import './ReverseString.js';
 // import './MergeSortedArray.js';
 // import './HashTables/HashTablesArray.js';
-import './HashTables/HashTablesObject.js';
+// import './HashTables/HashTablesObject.js';
+import './LinkedLists/SinglyLinkedLists.js';
 
 // console.log(result);
