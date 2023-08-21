99a6e7ca9e636621cbf802d597ac790524f99d46
Khoi Hoang <khoi.hoang@bouvet.no>
2023-08-21 10:29:19 +0200

    add: ✨ insertTail() for Linked Lists

--- public/src/LinkedLists/SinglyLinkedLists.js
+++ public/src/LinkedLists/SinglyLinkedLists.js
@@ -29,6 +29,24 @@ class SinglyLinkedLists {
 		return this;
 	}
 
+	insertTail(value) {
+		const tailNode = {
+			value: value,
+			id: this.#createMemorySpaceAllocation(),
+			nextNode: null,
+		};
+
+		let currentNode = this.head;
+
+		while (currentNode.nextNode !== null) {
+			currentNode = currentNode.nextNode;
+		}
+
+		currentNode.nextNode = tailNode;
+
+		this.length++;
+	}
+
 	#createMemorySpaceAllocation(minValue = 1000, maxValue = 9999) {
 		const min = minValue;
 		const max = maxValue;
@@ -40,5 +58,7 @@ class SinglyLinkedLists {
 const singlyLinkedLists = new SinglyLinkedLists(2);
 
 singlyLinkedLists.insertNextNode(5).insertNextNode(10).insertNextNode(20);
+
+singlyLinkedLists.insertTail(999);
 
 console.log(singlyLinkedLists);
