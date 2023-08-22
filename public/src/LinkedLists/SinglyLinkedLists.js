3efd09927d9fc29fb5872291708531ec5689a99f
Khoi Hoang <khoi.hoang@bouvet.no>
2023-08-22 10:57:16 +0200

    add: ✨ prepend for singly linked lists

--- public/src/LinkedLists/SinglyLinkedLists.js
+++ public/src/LinkedLists/SinglyLinkedLists.js
@@ -1,14 +1,16 @@
 class SinglyLinkedLists {
 	constructor(value) {
 		this.head = this.#createNewNode(value);
-
-		this.tail = this.head; // Initialize tail to head
+		this.tail = this.head; // this will be our reference to create nested nextNode
 		this.length = 0;
-		this._nodeCollections = {};
 	}
 
 	prependNode(value) {
-		const currentNode = this.head;
+		const newNode = this.#createNewNode(value);
+
+		newNode.nextNode = this.head;
+		this.head = newNode;
+		this.length++;
 	}
 
 	appendNode(value) {
@@ -16,39 +18,17 @@ class SinglyLinkedLists {
 
 		const newNode = this.#createNewNode(value);
 
-		this.#appendNextNode(newNode);
-		this.#appendNewTailNode(newNode);
-		this.#appendNewNodeCollections(newNode);
+		this.tail.nextNode = newNode;
+		this.tail = newNode;
 
 		return this;
 	}
 
-	#appendNewNodeCollections(newNode) {
-		this._nodeCollections[this.length] = newNode;
-	}
-
-	#appendNextNode(newNode) {
-		this.tail.nextNode = newNode;
-	}
-
-	#appendNewTailNode(newNode) {
-		this.tail = newNode;
-	}
-
 	#createNewNode(value) {
 		return {
-			id: this.#createMemorySpaceAllocation(),
 			value: value,
 			nextNode: null,
-			length: this.length ?? 0,
 		};
-	}
-
-	#createMemorySpaceAllocation(minValue = 1000, maxValue = 9999) {
-		const min = minValue;
-		const max = maxValue;
-		const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-		return randomNumber;
 	}
 }
 
@@ -56,5 +36,7 @@ const singlyLinkedLists = new SinglyLinkedLists(2);
 
 singlyLinkedLists.appendNode(5);
 singlyLinkedLists.appendNode(51).appendNode(100);
+
+singlyLinkedLists.prependNode(9);
 
 console.log(singlyLinkedLists);
