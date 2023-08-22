a7f055b434f5b3bb737a30bc2513240ade126b52
Khoi Hoang <khoi.hoang@bouvet.no>
2023-08-22 16:12:41 +0200

    init: 🎉 doubly linked lists

---
+++ public/src/LinkedLists/DoublyLinkedLists.js
@@ -0,0 +1,16 @@
+class DoublyLinkedLists {}
+
+const doublyLinkedLists = new DoublyLinkedLists();
+
+// doublyLinkedLists.appendNode(2);
+// doublyLinkedLists.appendNode(3);
+// doublyLinkedLists.appendNode(4);
+// doublyLinkedLists.appendNode(5);
+
+// doublyLinkedLists.prependNode(9);
+// doublyLinkedLists.insert(5, 20);
+// doublyLinkedLists.delete(3);
+
+// doublyLinkedLists.getReverseNodeCollections();
+
+console.log(doublyLinkedLists);
